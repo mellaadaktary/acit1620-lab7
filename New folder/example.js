80fe701434cdc03033c896596f801f38864fd2b9
mellaad aktary <mellaadaktary21@gmail.com>
2024-03-04 11:11:01 -0800

    first changes1

---
+++ New folder/example.js
@@ -0,0 +1,21 @@
+let num = 10;
+
+function isEven(num ){
+    return true;
+}
+
+if(isEven(num)){
+    console.log('the number is even')
+ } else {
+    console.log('the number is odd')
+ }
+let counter = 0;
+let sum = 0;
+
+
+while (counter <= 10){
+    sum = sum + counter;
+    counter = counter +1;
+
+}
+console.log(sum);
---
+++ New folder/index.js
@@ -0,0 +1,4 @@
+import {add} from './library';
+
+const heading = document.querySelector('h1');
+heading.textContent = 'Adding 10 and 20 equals' + '' + add (10,20);
